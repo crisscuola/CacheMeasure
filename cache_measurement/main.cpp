5f383a175fa920fed6475de6657098069db88709
Konstantin Ermakov <bestwick91@gmail.com>
2016-03-31 18:10:28 +0300

    Initial Commit

---
+++ cache_measurement/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cache_measurement
+//
+//  Created by Konstantin Ermakov on 31.03.16.
+//  Copyright © 2016 Konstantin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
